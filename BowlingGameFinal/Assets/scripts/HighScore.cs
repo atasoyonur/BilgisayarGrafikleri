205132163085e072783422e398ad0e3adebc28a5
atasoyonur <96890793+atasoyonur@users.noreply.github.com>
2022-12-26 22:24:42 +0300

    Delete BowlingGameFinal directory

--- BowlingGameFinal/Assets/scripts/HighScore.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HighScore", menuName = "HighScore", order = 1)]
-public class HighScore : ScriptableObject
-{
-    public int highScore;
-}
